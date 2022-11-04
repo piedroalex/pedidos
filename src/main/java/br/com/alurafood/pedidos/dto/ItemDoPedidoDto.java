b779b9a9ccba75b532f29c6e03f21d33b766c98f
Pedro Alex <piedroalex@gmail.com>
2022-11-04 15:24:41 -0300

    DTOs criados.

---
+++ src/main/java/br/com/alurafood/pedidos/dto/ItemDoPedidoDto.java
@@ -0,0 +1,15 @@
+package br.com.alurafood.pedidos.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter @Setter @AllArgsConstructor @NoArgsConstructor
+public class ItemDoPedidoDto {
+
+    private Long id;
+    private Integer quantidade;
+    private String descricao;
+    
+}
